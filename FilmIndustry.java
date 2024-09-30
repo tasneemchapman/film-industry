4e3c367cc861604375ad84b3a178d869ea9cb515
tasneemchapman <145393906+tasneemchapman@users.noreply.github.com>
2024-09-30 13:47:26 -0400

    created director record

--- FilmIndustry.java
+++ FilmIndustry.java
@@ -1,6 +1,16 @@
 public class FilmIndustry {
 
-    public interface FilmIndustryWorker {
-
+    public enum MovieGenre {
+        Horror, SciFi, Romance, Comedy, Drama, Action
     }
+
+    public interface FilmIndustryWorker {
+        // abtract methods
+    }
+    String firstName();
+
+    public record Director(String firstName, String lastName, int numYearsActive,
+                           boolean shootDigitalFilm) {
+    }
+
 }
